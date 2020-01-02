aba7fa7130bbca602f2870f49c0c43d040aab98b
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-02 03:04:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,11 +28,11 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[o]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[array.length -1]
 end
 
 def update_element_from_index(array, index_number, element)
